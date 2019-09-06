af7690d66d4792bc438f5c2873d0591a7c504b8a
IA <331044216@qq.com>
2019-09-06 20:37:48 +0800

    9.6晚上，张嘉鑫,commit，改了buildingDetail。

--- src/main/java/org/csu/lovelyhome/service/impl/FiltHuxingServiceImpl.java
+++ src/main/java/org/csu/lovelyhome/service/impl/FiltHuxingServiceImpl.java
@@ -4,7 +4,10 @@ import org.csu.lovelyhome.entity.FiltHuxing;
 import org.csu.lovelyhome.mapper.FiltHuxingMapper;
 import org.csu.lovelyhome.service.IFiltHuxingService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +20,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class FiltHuxingServiceImpl extends ServiceImpl<FiltHuxingMapper, FiltHuxing> implements IFiltHuxingService {
 
+    @Autowired
+    private FiltHuxingMapper filtHuxingMapper;
+
+    public List<String> getAllPositionNames(){
+        return filtHuxingMapper.getAllPositionNames();
+    }
+
+    public List<String> getAllAvePrices(){
+        return filtHuxingMapper.getAllAvePrices();
+    }
+
+    public List<String> getAllTotalPrices(){
+        return filtHuxingMapper.getAllTotalPrices();
+    }
+
+    public List<String> getAllAreas(){
+        return filtHuxingMapper.getAllAreas();
+    }
 }
